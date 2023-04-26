7088ad483bf8dd047c4dc817678ca3eddca94938
Vitaliy <109512360+VMeloshnikov@users.noreply.github.com>
2023-04-26 17:34:22 +0700

    Add files via upload

---
+++ HomeWorkHiberAston/src/main/java/com/example/HomeWorkHiberAston/HomeWorkHiberAstonApplication.java
@@ -0,0 +1,13 @@
+package com.example.HomeWorkHiberAston;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeWorkHiberAstonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeWorkHiberAstonApplication.class, args);
+	}
+
+}
---
+++ HomeWorkHiberAston/src/test/java/com/example/HomeWorkHiberAston/HomeWorkHiberAstonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.HomeWorkHiberAston;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeWorkHiberAstonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
